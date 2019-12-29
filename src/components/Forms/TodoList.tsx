2ba9fa5edf88c46990b62e76875f402fcaa03856
Bichoy Messiha <Bichoy.messiha@gmail.com>
2019-12-29 15:47:57 +0100

    refactor tree

---
+++ src/components/Forms/TodoList.tsx
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+const Todo: React.FC<any> = props => (
+    <tr>
+        <td>{props.todo.id}</td>
+        <td>{props.todo.description}</td>
+        <td>{props.todo.responsible}</td>
+        <td>{props.todo.priority}</td>
+        <td>{props.todo.isComplete}</td>
+        <td>
+            {" "}
+            <Link to={"/edit" + props.todo.id}>Edit</Link>
+        </td>
+    </tr>
+);
+
+class TodoList extends Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            todos: [],
+            isLoaded: false,
+        };
+    }
+    componentDidMount = async () => {
+        await fetch("http://localhost:4000/list")
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    todos: json.data,
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    };
+    componentDidUpdate = async () => {
+        await fetch("http://localhost:4000/list")
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    todos: json.data,
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    };
+
+    getTodosList = () => {
+        return (
+            this.state.todos &&
+            this.state.todos.map((todoz: any) => {
+                return <Todo key={todoz.id} todo={todoz} />;
+            })
+        );
+    };
+
+    render() {
+        return (
+            <div className="content-fluid">
+                <h2 className="text-center">Todo List</h2>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Responsible</th>
+                            <th scope="col">Priority</th>
+                            <th scope="col">Completed</th>
+                        </tr>
+                    </thead>
+                    <tbody>{this.getTodosList()}</tbody>
+                </table>
+            </div>
+        );
+    }
+}
+export default TodoList;
---
+++ src/components/Forms/adminLogin.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as yup from "yup";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+
+const validationSchema = yup.object().shape({
+    username: yup.string().required(),
+    password: yup.string().required(),
+});
+
+const adminLogin: React.FC = () => {
+    return (
+        <div>
+            <Formik
+                initialValues={{
+                    username: "",
+                    password: "",
+                }}
+                validationSchema={validationSchema}
+                onSubmit={async values => {
+                    //Lama 3mlt async await batal y3ml reset lel form
+                    axios.post("http://localhost:4001/", values).then(res => console.log(res.data));
+                    setTimeout(() => {
+                        console.log("logging the values", values);
+                    }, 250);
+                    return <Redirect to="/edit" />;
+                }}
+            >
+                {({ values, isSubmitting }) => (
+                    <Form>
+                        <div className="form-group col-md-6">
+                            <h3 className="text-left mt-2">
+                                You must Login as admin to Edit todos.
+                            </h3>
+                            <label>Username</label>
+                            <Field
+                                type="text"
+                                name="username"
+                                className="form-control"
+                                placeholder="Enter Todo description"
+                            />
+                            <ErrorMessage name="username">
+                                {msg => <div className="text-danger mt-2">{msg}</div>}
+                            </ErrorMessage>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label>Password</label>
+                            <Field
+                                type="password"
+                                name="password"
+                                className="form-control"
+                                placeholder="Enter Todo responsible"
+                            />
+                            <ErrorMessage name="password">
+                                {msg => <div className="text-danger mt-2">{msg}</div>}
+                            </ErrorMessage>
+                        </div>
+                        <button
+                            type="submit"
+                            disabled={isSubmitting}
+                            className="btn btn-outline-primary mt-2 ml-3"
+                        >
+                            Register
+                        </button>
+                        {/* This line is only used during debugging. */}
+                        {/* <pre className="mt-2">{JSON.stringify(values, null, 2)}</pre> */}
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+};
+
+export default adminLogin;
